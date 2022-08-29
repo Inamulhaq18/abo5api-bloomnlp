72e678037463521574bb100ec74787e37d186c54
Syed Inaamul Haq <37792859+Inamulhaq18@users.noreply.github.com>
2022-08-29 13:24:19 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -19,7 +19,19 @@ def return_status():
     if len(productname)==0:
         return jsonify(str("No name"))
     productname=""
+    return jsonify(str(responsea))
+
+@app.route('/generateinstant')
+def return_status():
+    """Return first the response and tie the my_task to a thread"""
+    productname = (request.args.get('productname'))
+    if len(productname)>0:
+        my_task(productname)
+    if len(productname)==0:
+        return jsonify(str("No name"))
+    productname=""
     return jsonify(str("Images detected"))
+
 
 def my_task(productname):
     print("rurl Before:  "+productname)
@@ -27,4 +39,4 @@ def my_task(productname):
     print("rurl After:  "+productname)
     print("_________________________")
     print(responsea)
-    return print('large function completed')
+    return (responsea)
